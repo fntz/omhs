5243f5ff4228c95111402354176e45e5baf65021
mike <mike.fch1@gmail.com>
2021-03-23 01:07:50 +0300

    remove unused classes (#10)

--- src/main/scala/AggregatedHttp2Message.scala
+++
@@ -1,9 +0,0 @@
-import io.netty.handler.codec.http2.{DefaultHttp2DataFrame, DefaultHttp2HeadersFrame, Http2FrameStream}
-
-case class AggregatedHttp2Message(
-                                   streamId: Int,
-                                   stream: Http2FrameStream,
-                                   data: DefaultHttp2DataFrame,
-                                  headers: DefaultHttp2HeadersFrame) {
-
-}
